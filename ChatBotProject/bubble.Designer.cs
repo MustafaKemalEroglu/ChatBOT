8ad3a2fe85463baa99978316e626399bcd10706d
mustafaeroglu32 <46123229+mustafaeroglu32@users.noreply.github.com>
2019-10-14 22:18:04 +0300

    0

---
+++ ChatBotProject/bubble.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ChatBotProject
+{
+    partial class bubble
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.lblmessage = new System.Windows.Forms.Label();
+            this.LabelMessageOwner = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 40;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblmessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblmessage.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmessage.ForeColor = System.Drawing.Color.White;
+            this.lblmessage.Location = new System.Drawing.Point(28, 13);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(720, 52);
+            this.lblmessage.TabIndex = 2;
+            this.lblmessage.Text = "Mesaj bu baloncuk içerisinde görünecek.";
+            // 
+            // LabelMessageOwner
+            // 
+            this.LabelMessageOwner.AutoSize = true;
+            this.LabelMessageOwner.ForeColor = System.Drawing.Color.Red;
+            this.LabelMessageOwner.Location = new System.Drawing.Point(19, 0);
+            this.LabelMessageOwner.Name = "LabelMessageOwner";
+            this.LabelMessageOwner.Size = new System.Drawing.Size(84, 13);
+            this.LabelMessageOwner.TabIndex = 3;
+            this.LabelMessageOwner.Text = "Message Owner";
+            // 
+            // bubble
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.LabelMessageOwner);
+            this.Controls.Add(this.lblmessage);
+            this.Name = "bubble";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.Size = new System.Drawing.Size(748, 70);
+            this.Load += new System.EventHandler(this.bubble_Load);
+            this.Resize += new System.EventHandler(this.bubble_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Label lblmessage;
+        public System.Windows.Forms.Label LabelMessageOwner;
+    }
+}
